10e87c04352de3eed167afb1410061aa42eb6a55
nicolazgarcia <nicolazgarcia@gmail.com>
2012-11-06 03:27:33 -0500

    push inicial para el monocromo
    espero haber hecho bien las cosas y la integracion con wp sea muy agil
    :)

---
+++ js/monocromo.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  var $filterType = $('#filter li a.active').attr('data-filter');
+  var $holder = $('#portfolio');
+  var $data = $holder.clone();
+	$('#filter li a').click(function(e) {
+		$('#filter li a').removeClass('active');
+		var $filterType = $(this).attr('data-filter');
+		$(this).addClass('active');
+		
+		if ($filterType == 'all') {
+			var $filteredData = $data.find('article');
+		} 
+		else {
+			var $filteredData = $data.find('article[data-type=' + $filterType + ']');
+		}
+		
+		// call quicksand and assign transition parameters
+		$holder.quicksand($filteredData, {
+			duration: 800
+		});
+		return false;
+	});	
+    $('.slideshow').each(function() {
+    	var $this = $(this);
+    	$this.cycle({
+		    containerResize: false,
+          	slideResize: false,
+			speed:    1000,
+			timeout:  0,
+			fx:       'fade',
+			next:     '.next',
+			prev: 	  '.prev'
+	    });
+	});
+	$('.about_slideshow').cycle({
+		containerResize: false,
+        slideResize: false,
+		fx: 	 'fade',
+		speed:   1000,
+		timeout: 3000,
+		next:    '.about'
+	});
+	var size = function() {
+       var newHeight = $(window).height();
+        $('article.project').height(newHeight);
+     }
+	$(window).on('resize', size);
+    size();
+	
+	var next;
+	$('.nextproject').click(function() {
+    	if ( next === undefined ) {
+        next = $('.project').next();
+    	} else {
+         next = next.next(); 
+   	 }
+    $('html,body').scrollTo(next , 1000, {margin:true} );
+	return false;
+	});
+	
+	$('.prev, .next').click(function () {
+		var $parent = $(this).parents('.project');					
+		$.scrollTo($parent, 1000);
+		
+	});
+
+
+   /* 
+   $('.slideshow').cycle({  
+        fx: 'fade',
+		speed:  500,
+        timeout: 0,
+        next: '#next',  
+        prev: '#prev'  
+    });
+	*/
+});
